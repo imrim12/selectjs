c693c269a1cc9f84219700dc74f8e87943218744
Rim (Y Nguyen) <46400321+imrim12@users.noreply.github.com>
2024-03-26 09:08:05 +0000

    chore: add package info

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 
+import dts from 'vite-plugin-dts'
+
 export default defineConfig({
   build: {
     lib: {
       name: 'SelectJS',
       entry: 'src/index.ts',
+      formats: ['cjs', 'es', 'umd']
     },
-  }
+  },
+  plugins: [
+    dts({
+      entryRoot: 'src',
+      outDir: 'dist'
+    })
+  ]
 })
