48d3eaef58cac0c8e17fe3e5512d52d0f65733bb
Rim (Y Nguyen) <46400321+imrim12@users.noreply.github.com>
2024-03-31 15:51:23 +0000

    fix: cannot remove content on input or textarea

--- src/remove.ts
+++ src/remove.ts
@@ -1,10 +1,27 @@
-export function removeSelectionContent() {
-  const selection = window.getSelection()
-  if (!selection?.rangeCount)
-    return
+import { isInputOrTextarea } from './utils'
 
-  const range = selection.getRangeAt(0)
-  range.deleteContents()
+interface RemoveSelectionContentOptions {
+  start: number
+  end: number
+}
 
-  return range
+export function removeSelectionContent(element: HTMLElement, options: RemoveSelectionContentOptions) {
+  if (isInputOrTextarea(element)) {
+    const _inputOrTextarea = element as HTMLInputElement | HTMLTextAreaElement
+
+    const beforeSelection = _inputOrTextarea.value.substring(0, options.start)
+    const afterSelection = _inputOrTextarea.value.substring(options.end, _inputOrTextarea.value.length)
+
+    _inputOrTextarea.value = beforeSelection + afterSelection
+  }
+  else {
+    const selection = window.getSelection()
+    if (!selection?.rangeCount)
+      return
+
+    const range = selection.getRangeAt(0)
+    range.deleteContents()
+
+    return range
+  }
 }
